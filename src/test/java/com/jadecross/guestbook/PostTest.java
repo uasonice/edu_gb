2dbcf602504862e200044119d2c0a14bf8e99bd1
devops <uasonice@gmail.com>
2022-10-12 16:18:24 +0900

    bug-fix

--- src/test/java/com/jadecross/guestbook/PostTest.java
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -1,6 +1,6 @@
 package com.jadecross.guestbook;
 
-import static org.junit.jupiter.api.Assertions.assetEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class PostTest {
